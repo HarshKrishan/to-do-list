e6cc885d311362da39b42249e60d9ab9f28bdec6
HarshKrishan <krishanharsh82@gmail.com>
2022-02-12 19:22:39 +0530

    Updates

---
+++ precache-manifest.5ae72ceda5f9b033c9a907749b360a8d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "153615e382c660cda24d96497846f639",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "7a678d656d5789954fff",
+    "url": "/to-do-list/static/js/2.43ab8c78.chunk.js"
+  },
+  {
+    "revision": "5cccb042a1a65faa6bd7",
+    "url": "/to-do-list/static/js/main.e9e7ba34.chunk.js"
+  },
+  {
+    "revision": "c5b49eebbffb21c94adb",
+    "url": "/to-do-list/static/js/runtime-main.6169b897.js"
+  }
+]);
